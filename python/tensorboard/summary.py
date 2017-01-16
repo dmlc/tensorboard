2701dd755601cc3f121a19a299a65038d7e84ccb
zihaolucky <zihaolucky@gmail.com>
2017-01-16 19:35:00 +0800

    Add image summary.

--- python/tensorboard/summary.py
+++ python/tensorboard/summary.py
@@ -36,6 +36,8 @@ from __future__ import print_function
 import logging
 import re as _re
 import bisect
+import cv2
+import numpy as np
 
 # pylint: disable=unused-import
 from .src.summary_pb2 import Summary
@@ -142,53 +144,49 @@ def make_histogram(values):
                           bucket=bucket)
 
 
+def image(tag, tensor):
+    """Outputs a `Summary` protocol buffer with images.
+    The summary has up to `max_images` summary values containing images. The
+    images are built from `tensor` which must be 3-D with shape `[height, width,
+    channels]` and where `channels` can be:
+    *  1: `tensor` is interpreted as Grayscale.
+    *  3: `tensor` is interpreted as RGB.
+    *  4: `tensor` is interpreted as RGBA.
+    The `name` in the outputted Summary.Value protobufs is generated based on the
+    name, with a suffix depending on the max_outputs setting:
+    *  If `max_outputs` is 1, the summary value tag is '*name*/image'.
+    *  If `max_outputs` is greater than 1, the summary value tags are
+       generated sequentially as '*name*/image/0', '*name*/image/1', etc.
+    Args:
+      tag: A name for the generated node. Will also serve as a series name in
+        TensorBoard.
+      tensor: A 3-D `uint8` or `float32` `Tensor` of shape `[height, width,
+        channels]` where `channels` is 1, 3, or 4.
+    Returns:
+      A scalar `Tensor` of type `string`. The serialized `Summary` protocol
+      buffer.
+    """
+    tag = _clean_tag(tag)
+    if not isinstance(tensor, np.ndarray):
+        # try conversion, if failed then need handle by user.
+        tensor = np.ndarray(tensor, dtype=np.float32)
+    shape = tensor.shape
+    height, width, channel = shape[0], shape[1], shape[2]
+    image = make_image(tensor, height, width, channel)
+    return Summary(value=[Summary.Value(tag=tag, image=image)])
+
+
+def make_image(tensor, height, width, channel):
+    """Convert an numpy representation image to Image protobuf"""
+    image_string = cv2.imencode('.png', tensor)[1].tostring()
+    return Summary.Image(height=height,
+                         width=width,
+                         colorspace=channel,
+                         encoded_image_string=image_string)
+
+
+
 '''TODO(zihaolucky). support more summary types later.
-def image(name, tensor, max_outputs=3, collections=None):
-  """Outputs a `Summary` protocol buffer with images.
-  The summary has up to `max_images` summary values containing images. The
-  images are built from `tensor` which must be 4-D with shape `[batch_size,
-  height, width, channels]` and where `channels` can be:
-  *  1: `tensor` is interpreted as Grayscale.
-  *  3: `tensor` is interpreted as RGB.
-  *  4: `tensor` is interpreted as RGBA.
-  The images have the same number of channels as the input tensor. For float
-  input, the values are normalized one image at a time to fit in the range
-  `[0, 255]`.  `uint8` values are unchanged.  The op uses two different
-  normalization algorithms:
-  *  If the input values are all positive, they are rescaled so the largest one
-     is 255.
-  *  If any input value is negative, the values are shifted so input value 0.0
-     is at 127.  They are then rescaled so that either the smallest value is 0,
-     or the largest one is 255.
-  The `tag` in the outputted Summary.Value protobufs is generated based on the
-  name, with a suffix depending on the max_outputs setting:
-  *  If `max_outputs` is 1, the summary value tag is '*name*/image'.
-  *  If `max_outputs` is greater than 1, the summary value tags are
-     generated sequentially as '*name*/image/0', '*name*/image/1', etc.
-  Args:
-    name: A name for the generated node. Will also serve as a series name in
-      TensorBoard.
-    tensor: A 4-D `uint8` or `float32` `Tensor` of shape `[batch_size, height,
-      width, channels]` where `channels` is 1, 3, or 4.
-    max_outputs: Max number of batch elements to generate images for.
-    collections: Optional list of ops.GraphKeys.  The collections to add the
-      summary to.  Defaults to [_ops.GraphKeys.SUMMARIES]
-  Returns:
-    A scalar `Tensor` of type `string`. The serialized `Summary` protocol
-    buffer.
-  """
-  name = _clean_tag(name)
-  with _ops.name_scope(name, None, [tensor]) as scope:
-    # pylint: disable=protected-access
-    val = _gen_logging_ops._image_summary(
-        tag=scope.rstrip('/'),
-        tensor=tensor,
-        max_images=max_outputs,
-        name=scope)
-    _collect(val, collections, [_ops.GraphKeys.SUMMARIES])
-  return val
-
-
 def audio(name, tensor, sample_rate, max_outputs=3, collections=None):
   # pylint: disable=line-too-long
   """Outputs a `Summary` protocol buffer with audio.
