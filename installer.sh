717d437cb6475d2afbf245c50146d42ac2aed884
zihaolucky <zihaolucky@gmail.com>
2017-02-24 00:27:04 +0800

    improve installer, fix issues in linux/ubuntu.

--- installer.sh
+++ installer.sh
@@ -7,15 +7,15 @@ make all
 git clone https://github.com/tensorflow/tensorflow
 cd tensorflow
 
-# run configuration. TODO. how to automate configure in travis-ci?
-sh ./configure
+# run configuration.
+bash configure
 # build tensorboard
 bazel build tensorflow/tensorboard:tensorboard
 
 # prepare pip installation package
 cp -r ../tools/* bazel-bin/tensorflow/tools/
 # get .whl file in python/dist/
-sh bazel-bin/tensorflow/tools/pip_package/build_pip_package.sh ../python/dist/
+bash bazel-bin/tensorflow/tools/pip_package/build_pip_package.sh ../python/dist/
 
 # install tensorboard package from .whl file
 cd ..
